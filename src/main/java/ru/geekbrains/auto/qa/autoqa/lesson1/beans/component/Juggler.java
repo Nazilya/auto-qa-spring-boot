d42a8e93b26545a3297b284c578013723bd452a1
Nazilya <Nazilya>
2022-10-16 04:35:30 +0500

    lesson1

---
+++ src/main/java/ru/geekbrains/auto/qa/autoqa/lesson1/beans/component/Juggler.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.auto.qa.autoqa.lesson1.beans.component;
+import org.springframework.stereotype.Component;
+@Component
+public class Juggler {
+
+    private String name;
+    private String secondName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    //logic code
+}
---
+++ src/main/java/ru/geekbrains/auto/qa/autoqa/lesson1/dependency/hardness/Car.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.auto.qa.autoqa.lesson1.dependency.hardness;
+
+import java.lang.reflect.Field;
+
+public class Car {
+    private Engine engine;
+
+    public Car() {
+        engine = new Engine();
+    }
+
+}
+
+
